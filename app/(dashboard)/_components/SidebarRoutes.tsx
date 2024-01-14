0331e199a3e3668fe55894f9e667279500c46cfa
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-14 22:02:04 +0200

    add sideBar

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,10 +1,48 @@
+"use client"
+import { BarChart, Compass, Layout, List } from 'lucide-react';
 import React from 'react'
+import SidebarItem from './SidebarItem';
 
 type Props = {}
+const guestRoutes = [
+    {
+      icon: Layout,
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      icon: Compass,
+      label: "Browse",
+      href: "/search",
+    },
+  ];
+  
+  const teacherRoutes = [
+    {
+      icon: List,
+      label: "Courses",
+      href: "/teacher/courses",
+    },
+    {
+      icon: BarChart,
+      label: "Analytics",
+      href: "/teacher/analytics",
+    },
+  ]
 
 const SidebarRoutes = (props: Props) => {
+    const routes = guestRoutes;
   return (
-    <div>SidebarRoutes</div>
+    <div className="flex flex-col w-full">
+       {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
   )
 }
 
