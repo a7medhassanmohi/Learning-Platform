83b5122c5663801adac90c4febb82a77c14fffcb
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-15 14:00:29 +0200

    add form for create new course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,10 +1,18 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <div className='p-6'>
+      <Link href="/teacher/create">
+        <Button className='p-6'>
+          new course
+        </Button>
+      </Link>
+    </div>
   )
 }
 
