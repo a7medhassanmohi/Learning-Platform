52d85a16110f5c4d097a74afc0cd84a7bee3a1de
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-16 21:50:18 +0200

    add course edit page ui

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import { IconBadge } from "@/components/IconBadge";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
 
 type Props = {
-    params:{courseId:string}
-}
+  params: { courseId: string };
+};
 
-const CourseIdPage = ({params}: Props) => {
+const CourseIdPage = async ({ params }: Props) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      userId,
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+  const isComplete = requiredFields.every(Boolean);
+
   return (
-    <div>CourseIdPage {params.courseId}</div>
-  )
-}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+             <h1 className="text-2xl font-medium">
+              Course setup
+            </h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completionText}
+            </span>  
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+        <div className="flex items-center gap-x-2">
+        <IconBadge icon={LayoutDashboard}  />
+        <h2 className="text-xl">
+                Customize your course
+              </h2>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CourseIdPage
+export default CourseIdPage;
