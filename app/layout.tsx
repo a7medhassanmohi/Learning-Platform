f315455cf83fb7256d0d1c75006eec455c54d166
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-18 16:04:59 +0200

    new project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ClerkProvider } from "@clerk/nextjs";
 import './globals.css'
-import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,15 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-
     <html lang="en">
-      <body className={inter.className}>
-      <Toaster />
-        {children}
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
-
-    </ClerkProvider>
   )
 }
--- schema/index.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-export const formSchema = z.object({
-    title: z.string().min(1, {
-      message: "Title is required",
-    }),
-  });
-  
-  export type FormSchemaType=z.infer<typeof formSchema>
